694632e8a0e09662bcf7c811941e8a7a5a7122db
dhakadrohan98 <rohandhakad20@gmail.com>
2024-08-29 17:01:00 +0530

    Delete department, Get all departments & get dept details by id & Update a particular department REST APIs are written for department project

--- restapi/src/main/java/com/wissen/restapi/controller/DepartmentController.java
+++ restapi/src/main/java/com/wissen/restapi/controller/DepartmentController.java
@@ -3,9 +3,9 @@ package com.wissen.restapi.controller;
 import com.wissen.restapi.entity.Department;
 import com.wissen.restapi.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -17,5 +17,37 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department) {
         return departmenService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList() {
+        return departmenService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        return departmenService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepatmentById(@PathVariable("id") Long departmentId) {
+        try {
+            boolean result = departmenService.deleteDepatmentById(departmentId);
+            if(result) {
+                return "Department is deleted successfully!";
+            } else {
+                return "Dept with given id doesn't exist";
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return "Dept with given id doesn't exist";
+    }
+
+    //i want dept id & dept object with updated details
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId,
+                                       @RequestBody Department department) {
+        return departmenService.updateDepartment(departmentId, department);
     }
 }
