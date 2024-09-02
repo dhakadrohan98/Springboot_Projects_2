b044534340ef1642677fae87dd49561daf07c105
dhakadrohan98 <rohandhakad20@gmail.com>
2024-09-02 21:06:13 +0530

    Two instances of a server has been created & round robin algorithm has been applied in client application to demonstrate the load balancer when there are multiple requests from instances of a same server

--- client/src/main/java/com/wissen/client/DemoInstanceSupplier.java
+++ client/src/main/java/com/wissen/client/DemoInstanceSupplier.java
@@ -8,6 +8,11 @@ import reactor.core.publisher.Flux;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ *  provide a list of service instances that are available
+ *  for a given service ID.
+ *  It defines how we find available service instances.
+ */
 public class DemoInstanceSupplier implements ServiceInstanceListSupplier {
 
     private final String serviceId;
@@ -15,25 +20,33 @@ public class DemoInstanceSupplier implements ServiceInstanceListSupplier {
     public DemoInstanceSupplier(String serviceId) {
         this.serviceId = serviceId;
     }
+
     @Override
     public String getServiceId() {
-        return serviceId;
+        return this.serviceId;
     }
 
     /**
-     *     String instanceId,
-     *     String serviceId,
-     *     String host,
-     *     int port,
-     *     boolean secure
+     * String instanceId,
+     * String serviceId,
+     * String host,
+     * int port,
+     * boolean secure
+     */
+    /**
+     * Returns a Flux containing a list of service instances, each representing a mocked
+     * server instance with specific host and port.
+     *
+     * DefaultServiceInstance is the concrete implementation of the
+     * ServiceInstance interface
      */
     @Override
     public Flux<List<ServiceInstance>> get() {
         return Flux.just(Arrays
-                .asList(new DefaultServiceInstance(serviceId + "1",  serviceId,
-                        "localhost", 8086, false),
-                        new DefaultServiceInstance(serviceId + "2",  serviceId,
-                                "localhost", 8087, false)
+                .asList(new DefaultServiceInstance(serviceId + "1", serviceId,
+                                "localhost", 8080, false),
+                        new DefaultServiceInstance(serviceId + "2", serviceId,
+                                "localhost", 8081, false)
                 )
         );
     }
