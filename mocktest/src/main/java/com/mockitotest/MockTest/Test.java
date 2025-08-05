ad8f9aff196308a7a4f4cddf8f582ac65c422593
RohanDhakad <dhakadrohan98>
2025-08-05 13:33:43 +0530

    some code changea are being pushed after some months

---
+++ mocktest/src/main/java/com/mockitotest/MockTest/Test.java
@@ -0,0 +1,30 @@
+package com.mockitotest.MockTest;
+//1.
+//Input: s = "abccccdd"
+//o/p: 7
+//
+//a - 1
+//b - 1
+//cccc - 4
+//d - 2
+//
+//        "dccaccd"
+//
+//ccdbdcc
+
+/*
+2.
+Input: nums = [1], k = 1
+Output: 1
+
+ex-2
+nums[] = {1, 2}, k=4
+o/p: -1
+
+ex-3
+nums = {1, 2, 3, 2, 5, 7, 2}, k = 5
+o/p -
+*/
+
+public class Test {
+}
--- weather-api/src/main/java/com/weather/WeatherApplication.java
+++ weather-api/src/main/java/com/weather/WeatherApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.beans.container.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
